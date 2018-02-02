5232773364775c4ed0e12a0a77b8c18b46d3f387
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-02-02 19:33:30 +0900

    sample interface

---
+++ interface.py
@@ -0,0 +1,7 @@
+from pyArXiv.pyArXiv import pyArXiv
+
+print("Thanks for using pyArXiv!\n\n")
+paper_id = input("paper id : ")
+paper_info = pyArXiv.query(paper_id)
+print(paper_info)
+pyArXiv.download(paper_info)
