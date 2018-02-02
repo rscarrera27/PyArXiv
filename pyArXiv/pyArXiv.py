65b01a48c68b53dfd72b6d3457dbf5e3af5b3c40
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-02-02 18:45:41 +0900

    Complete id query

--- pyArXiv/pyArXiv.py
+++ pyArXiv/pyArXiv.py
@@ -1,10 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+
+
 class pyArXiv:
 
     def __init__(self):
         pass
 
-    def download(self, paper_info):
-        raise NotImplementedError
+    @staticmethod
+    def download(paper_info):
+        """
+        :param paper_info: {
+                            "paper_id": int,
+                            "paper_name": str
+                            }
+        :return: None
+        """
+    @staticmethod
+    def query(paper_id):
+        """
+        :param paper_id: int
+        :return: {
+                  "paper_id": int,
+                  "paper_name": str
+                  }
+        """
+        url = "https://arxiv.org/abs/{}".format(paper_id)
+        html = requests.get(url)
 
-    def query(self, paper_id):
-        raise NotImplementedError
+        if html.status_code != 200:
+            return ValueError
+
+        paper_name = str(list(BeautifulSoup(html.text, 'html.parser').find("h1", "title mathjax"))[1])
+        print(paper_name)
+
+        paper_info = {
+            "paper_id": paper_id,
+            "paper_name": paper_name
+        }
+
+        return paper_info
+
+
+
+ad = pyArXiv
+ad.query("1412.0035")
