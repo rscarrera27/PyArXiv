5d02dbec6617553664b1104c62e62852e153802b
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-02-02 18:01:28 +0900

    Update main structure

---
+++ pyArXiv/pyArXiv.py
@@ -0,0 +1,10 @@
+class pyArXiv:
+
+    def __init__(self):
+        pass
+
+    def download(self, paper_info):
+        raise NotImplementedError
+
+    def query(self, paper_id):
+        raise NotImplementedError
